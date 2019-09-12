4c55379fcf50ad98450274214131ff10cc3bb328
Yun Shan <yunshan21st@qq.com>
2019-09-12 14:12:16 +0800

    update angular cdk/material to 8

--- src/app/share/material.module.ts
+++ src/app/share/material.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
-import {
-  MatButtonModule, MatCardModule,
-  MatCheckboxModule, MatDividerModule, MatExpansionModule, MatFormFieldModule, MatGridListModule, MatIconModule,
-  MatInputModule, MatListModule,
-  MatOptionModule,
-  MatRadioModule,
-  MatSelectModule, MatSlideToggleModule,
-  MatToolbarModule, MatTooltipModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 const MATERIAL_MODULES = [
   MatButtonModule,
