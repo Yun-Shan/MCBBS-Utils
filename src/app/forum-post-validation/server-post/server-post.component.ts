c8ab2899733b7a35c42e5907b23a00612f14cf29
Yun Shan <yunshan21st@qq.com>
2019-12-18 14:48:59 +0800

    版本号增加1.15.1

--- src/app/forum-post-validation/server-post/server-post.component.ts
+++ src/app/forum-post-validation/server-post/server-post.component.ts
@@ -17,7 +17,7 @@ export class ServerPostComponent implements OnInit {
   server: Server = new Server();
   form: FormGroup;
 
-  versionList = ['1.15', '1.14.4', '1.14', '1.13.2', '1.13.1', '1.13', '1.12.2', '1.12.1', '1.12',
+  versionList = ['1.15.1', '1.15', '1.14.4', '1.14', '1.13.2', '1.13.1', '1.13', '1.12.2', '1.12.1', '1.12',
     '1.11.2', '1.11', '1.10.X', '1.9.4', '1.9', '1.8.X', '1.7.10', '1.7.2', '1.6.4'];
 
   constructor() {
